2621763057db85980b93ff54dfb3f04b00e987fc
MR KITTY <appu20080804@gmail.com>
2025-05-10 14:20:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=5pAXxYba#y1e1poflRyL0yFGnf-IZwaDrXFpyXmxjWoWR_6DYgJ0",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=F8p2iKoR#dovtcvgreSwLPDjPgmdxi_KjrZlz3xeRnUMJypPu8Cc",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
